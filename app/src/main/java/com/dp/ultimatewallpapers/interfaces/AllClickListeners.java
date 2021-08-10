f1e550cda8ef0297cef72a1f3ed1a57d115d87f7
Pujit <pujit.somaiya@internal.mail>
2021-08-10 12:50:40 +0530

    init commit

---
+++ app/src/main/java/com/dp/ultimatewallpapers/interfaces/AllClickListeners.java
@@ -0,0 +1,19 @@
+package com.dp.ultimatewallpapers.interfaces;
+
+import android.widget.RadioButton;
+
+import com.dp.ultimatewallpapers.api.response.HitsItem;
+
+public interface AllClickListeners {
+
+
+    interface OnImageClick{
+        void onImageClick(int position, HitsItem item);
+    }
+
+
+    interface SetOnBottomDialogButtonClick {
+        void setFilter(int number, RadioButton rbImageType, RadioButton rbOrientation);
+    }
+
+}
---
+++ app/src/main/java/com/dp/ultimatewallpapers/view/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.dp.ultimatewallpapers.view.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dp.ultimatewallpapers.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
